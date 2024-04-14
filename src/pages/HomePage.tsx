8321994ee593a703ef889f6503345310ab9f2744
Kevin Arias <aa-kevin@hotmail.com>
2024-04-14 12:42:36 -0600

    change styles

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -37,12 +37,12 @@ export const HomePage = () => {
       <img
         src={LeafsIcon}
         alt="leafs"
-        className="absolute top-52 w-96 opacity-20 sm:right-1/2 sm:top-114 2xl:right-[40%]"
+        className="absolute top-52 w-96 opacity-20 sm:right-1/2 2xl:right-[40%] h-xs:sm:top-80 h-lg:sm:top-114"
       />
 
-      <div className="flex grid-cols-2 flex-col-reverse gap-4 px-5 pt-12 text-lg font-light md:px-10 lg:px-32 2xl:px-40 h-sm:md:grid">
+      <div className="flex grid-cols-2 flex-col-reverse gap-4 px-5 pt-12 text-lg font-light md:px-10 2xl:px-40 h-xs:lg:grid h-md:lg:px-32">
         <animated.div
-          className="mb-16 text-justify text-green-300 h-sm:sm:mb-0 h-sm:md:text-white-50"
+          className="mb-16 text-justify text-green-300 h-xs:lg:mb-0 h-xs:lg:text-white-50"
           style={textAnimation}
         >
           <p className="mb-5">{t('home.paragraph1')}</p>
@@ -53,8 +53,8 @@ export const HomePage = () => {
           <animated.img
             style={bottle20Animation}
             src={Bottle20Anniversary}
-            className="-mr-10 h-full w-40 h-sm:sm:ml-auto h-sm:sm:h-fit h-sm:sm:w-96 h-sm:md:-mr-28 h-sm:lg:ml-28 h-sm:lg:w-60 h-sm:2xl:ml-40"
-            alt="Botella 30"
+            className="-mr-10 h-full w-40 h-xs:md:ml-0 h-xs:lg:h-fit h-sm:lg:ml-auto h-md:sm:w-96 h-md:md:-mr-28 h-md:lg:ml-28 h-md:lg:w-60 h-md:2xl:ml-40"
+            alt="Ron centenario 20"
           />
           <animated.img
             style={{
@@ -65,14 +65,14 @@ export const HomePage = () => {
               }),
             }}
             src={Bottle30Anniversary}
-            className="z-10 -mr-10 mt-28 h-full w-40 h-sm:sm:h-fit h-sm:sm:w-96 h-sm:md:-mr-28 h-sm:lg:w-60"
-            alt="Botella 30"
+            className="z-10 -mr-10 mt-28 h-full w-40 h-xs:lg:h-fit h-md:sm:w-96 h-md:md:-mr-28 h-md:lg:w-60"
+            alt="Ron centenario 30"
           />
           <animated.img
             style={bottle25Animation}
             src={Bottle25Anniversary}
-            className="h-full w-36 h-sm:sm:h-fit h-sm:sm:w-96 h-sm:lg:w-52"
-            alt="Botella 30"
+            className="h-full w-36 h-xs:lg:h-fit h-md:sm:w-96 h-md:lg:w-52"
+            alt="Ron centenario 25"
           />
         </div>
       </div>
